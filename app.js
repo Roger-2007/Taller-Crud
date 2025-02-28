8c0655376852e5cbccbb1d9a422eee4402790b91
Roger-2007 <164978282+Roger-2007@users.noreply.github.com>
2025-02-28 14:55:12 -0500

    ajustes

--- app.js
+++ app.js
@@ -1,129 +1,131 @@
-const { jsPDF }=window.jspdf;
+const { jsPDF } = window.jspdf;
 const doc = new jsPDF()
 
-const nombreProducto=document.getElementById("nombreProducto")
-const precioProducto=document.getElementById("precioProducto")
-const descripcionProducto=document.getElementById("descripcionProducto")
-const imagenProducto=document.getElementById("imagenProducto")
-const btnGuardar=document.getElementById("btnGuardar")
+const nombreProducto = document.getElementById("nombreProducto")
+const precioProducto = document.getElementById("precioProducto")
+const descripcionProducto = document.getElementById("descripcionProducto")
+const imagenProducto = document.getElementById("imagenProducto")
+const btnGuardar = document.getElementById("btnGuardar")
 const btnPdf = document.getElementById("btnPdf")
 const tabla = document.querySelector(".table tbody")
-const inputBuscador=document.getElementById("inputBuscador")
+const inputBuscador = document.getElementById("inputBuscador")
 
-btnGuardar.addEventListener("click",()=>{
-   
-   
+btnGuardar.addEventListener("click", () => {
+
+
     validarDatos()
     mostrarDatos()
 
-    
+
 })
-document.addEventListener("DOMContentLoaded",()=>{
+document.addEventListener("DOMContentLoaded", () => {
     mostrarDatos()
 })
 
-function validarDatos(){
+function validarDatos() {
     let producto
-    if (nombreProducto.value && precioProducto.value && descripcionProducto.value && imagenProducto.value){
-        
-     producto = {
-        nombre : nombreProducto.value,
-        precio : precioProducto.value,
-        descripcion : descripcionProducto.value,
-        imagen : imagenProducto.value
-    }
+    if (nombreProducto.value && precioProducto.value && descripcionProducto.value && imagenProducto.value) {
 
-nombreProducto.value=""
-precioProducto.value=""
-descripcionProducto.value=""
-imagenProducto.value=""
-guardarProducto(producto)
+        producto = {
+            nombre: nombreProducto.value,
+            precio: precioProducto.value,
+            descripcion: descripcionProducto.value,
+            imagen: imagenProducto.value
+        }
+
+        nombreProducto.value = ""
+        precioProducto.value = ""
+        descripcionProducto.value = ""
+        imagenProducto.value = ""
+        guardarProducto(producto)
+        alert("Se guardo exitosamente")
     }
-    else{
+    else {
         alert("Todos los campos son obligatorios")
         return
     }
-        
+
 
 }
 
-function guardarProducto(producto){
-let productoPrevio =JSON.parse( localStorage.getItem("productos")) ||[]
-productoPrevio.push(producto)
-localStorage.setItem("productos",JSON.stringify(productoPrevio))
+function guardarProducto(producto) {
+    let productoPrevio = JSON.parse(localStorage.getItem("productos")) || []
+    productoPrevio.push(producto)
+    localStorage.setItem("productos", JSON.stringify(productoPrevio))
 }
 
-function mostrarDatos(){
-   borrarTabla()
+function mostrarDatos() {
+    borrarTabla()
     let datos = JSON.parse(localStorage.getItem("productos")) || []
-    datos.forEach((dato,i)=> {
-       let fila = document.createElement("tr")
-       fila.innerHTML=
-        `<td class="align-middle">${i+1}</td>
-        <td class="align-middle">${dato.nombre}</td>
-        <td class="align-middle">${dato.precio}</td>
-        <td class="align-middle">${dato.descripcion}</td>
-<td class="align-middle"><img src="${dato.imagen}" width="30%"> </td>   
-<td class="align-middle"><button onclick="actualizarDatos(${i})" class="btn btn-primary btnEditar">📝</button>     
-<button onclick="eliminarDatos(${i})" class="btn btn-danger btnEliminar">❌</button></td>
-        `
+    datos.forEach((dato, i) => {
+        let fila = document.createElement("tr")
+        fila.innerHTML =
+            `<td class="align-middle">${i + 1}</td>
+            <td class="align-middle">${dato.nombre}</td>
+            <td class="align-middle">${dato.precio}</td>
+            <td class="align-middle">${dato.descripcion}</td>
+            <td class="align-middle"><img src="${dato.imagen}" width="30%"> </td>   
+            <td class="align-middle">
+                <button onclick="actualizarDatos(${i})" class="btn btn-primary btnEditar">📝</button>     
+                <button onclick="eliminarDatos(${i})" class="btn btn-danger btnEliminar">❌</button>
+            </td>`
         tabla.appendChild(fila)
     });
 
 }
-function borrarTabla(){
+function borrarTabla() {
     let datos = document.querySelectorAll("table tbody tr")
-    datos.forEach((dato)=>{
+    datos.forEach((dato) => {
         dato.remove()
     })
 }
 
-function eliminarDatos(i){
-    let productos = JSON.parse(localStorage.getItem("productos"))||[]
+function eliminarDatos(i) {
+    let productos = JSON.parse(localStorage.getItem("productos")) || []
     let confirmar = confirm(`¿Desea eliminar el producto: ${productos[i].nombre}`)
-    if(confirmar){
+    if (confirmar) {
         alert("Lo eliminaste")
-    productos.splice(i,1)
-    localStorage.setItem("productos",JSON.stringify(productos))
+        productos.splice(i, 1)
+        localStorage.setItem("productos", JSON.stringify(productos))
         mostrarDatos()
     }
 }
 
-function actualizarDatos(i){
-    let btnActualizar=document.getElementById("btnActualizar")
-    
+function actualizarDatos(i) {
+    let btnActualizar = document.getElementById("btnActualizar")
+
     btnGuardar.classList.toggle("d-none")
-btnActualizar.classList.toggle("d-none")
-    let productos = JSON.parse(localStorage.getItem("productos"))||[]
+    btnActualizar.classList.toggle("d-none")
+    let productos = JSON.parse(localStorage.getItem("productos")) || []
 
-nombreProducto.value = productos[i].nombre
-precioProducto.value = productos[i].precio
-descripcionProducto.value = productos[i].descripcion
-imagenProducto.value = productos[i].imagen
+    nombreProducto.value = productos[i].nombre
+    precioProducto.value = productos[i].precio
+    descripcionProducto.value = productos[i].descripcion
+    imagenProducto.value = productos[i].imagen
 
-btnActualizar.replaceWith(btnActualizar.cloneNode(true));
-btnActualizar = document.getElementById("btnActualizar");
+    btnActualizar.replaceWith(btnActualizar.cloneNode(true));
+    btnActualizar = document.getElementById("btnActualizar");
 
-btnActualizar.addEventListener("click",function(){
-    productos[i].nombre = nombreProducto.value 
-    productos[i].precio = precioProducto.value
-    productos[i].descripcion = descripcionProducto.value
-    productos[i].imagen = imagenProducto.value
-    localStorage.setItem("productos",JSON.stringify(productos))
-  
-
-alert("Se actualizaron los datos con exito")
+    btnActualizar.addEventListener("click", function () {
+        productos[i].nombre = nombreProducto.value
+        productos[i].precio = precioProducto.value
+        productos[i].descripcion = descripcionProducto.value
+        productos[i].imagen = imagenProducto.value
+        localStorage.setItem("productos", JSON.stringify(productos))
 
 
-  nombreProducto.value = ""
-precioProducto.value = ""
-descripcionProducto.value = ""
-imagenProducto.value = ""   
+        alert("Se actualizaron los datos con exito")
 
-btnGuardar.classList.toggle("d-none")
-btnActualizar.classList.toggle("d-none")
-mostrarDatos()
-})
+
+        nombreProducto.value = ""
+        precioProducto.value = ""
+        descripcionProducto.value = ""
+        imagenProducto.value = ""
+
+        btnGuardar.classList.toggle("d-none")
+        btnActualizar.classList.toggle("d-none")
+        mostrarDatos()
+    })
 }
 
 
@@ -131,68 +133,65 @@ mostrarDatos()
 let buscarPor = document.getElementById("buscarPor")
 
 
-let valorSelect=buscarPor.value
+let valorSelect = buscarPor.value
 
-buscarPor.addEventListener("change",()=>{
-  valorSelect = buscarPor.value
+buscarPor.addEventListener("change", () => {
+    valorSelect = buscarPor.value
 
 })
 
 
 
-inputBuscador.addEventListener("input",()=>{
+inputBuscador.addEventListener("input", () => {
 
-console.log(valorSelect);
     let inputBuscadorValue = inputBuscador.value.toLowerCase()
-console.log(inputBuscadorValue);
-let productos = JSON.parse(localStorage.getItem("productos"))||[]
-let productosBuscados
+    let productos = JSON.parse(localStorage.getItem("productos")) || []
+    let productosBuscados
 
 
-if(valorSelect==="nombre"){
- productosBuscados = productos.filter(producto => producto.nombre.toLowerCase().includes(inputBuscadorValue))
-console.log(productosBuscados);}
-else if(valorSelect==="precio"){
-     productosBuscados = productos.filter(producto => producto.precio.toLowerCase().includes(inputBuscadorValue))
-console.log(productosBuscados);
-}
-else if(valorSelect==="descripcion"){
-     productosBuscados = productos.filter(producto => producto.descripcion.toLowerCase().includes(inputBuscadorValue))
-console.log(productosBuscados);
-}
- 
+    if (valorSelect === "nombre") {
+        productosBuscados = productos.filter(producto => producto.nombre.toLowerCase().includes(inputBuscadorValue))
+    }
+    else if (valorSelect === "precio") {
+        productosBuscados = productos.filter(producto => producto.precio.toLowerCase().includes(inputBuscadorValue))
+    }
+    else if (valorSelect === "descripcion") {
+        productosBuscados = productos.filter(producto => producto.descripcion.toLowerCase().includes(inputBuscadorValue))
+    }
 
-let datos = document.querySelectorAll("table tbody tr")
-    datos.forEach((dato)=>{
+
+    let datos = document.querySelectorAll("table tbody tr")
+    datos.forEach((dato) => {
         dato.remove()
     })
 
-productosBuscados.forEach((dato,i)=> {
-    let fila = document.createElement("tr")
-    fila.innerHTML=
-     `<td class="align-middle">${i+1}</td>
-     <td class="align-middle">${dato.nombre}</td>
-     <td class="align-middle">${dato.precio}</td>
-     <td class="align-middle">${dato.descripcion}</td>
-<td class="align-middle"><img src="${dato.imagen}" width="30%"> </td>   
-<td class="align-middle"><button onclick="actualizarDatos(${i})" class="btn btn-primary btnEditar">📝</button>     
-<button onclick="eliminarDatos(${i})" class="btn btn-danger btnEliminar">❌</button></td>
-     `
-     tabla.appendChild(fila)
- });
+    productosBuscados.forEach((dato, i) => {
+        let fila = document.createElement("tr")
+        fila.innerHTML =
+            `<td class="align-middle">${i + 1}</td>
+            <td class="align-middle">${dato.nombre}</td>
+            <td class="align-middle">${dato.precio}</td>
+            <td class="align-middle">${dato.descripcion}</td>
+            <td class="align-middle"><img src="${dato.imagen}" width="30%"> </td>   
+            <td class="align-middle">
+                 <button onclick="actualizarDatos(${i})" class="btn btn-primary btnEditar">📝</button>     
+                 <button onclick="eliminarDatos(${i})" class="btn btn-danger btnEliminar">❌</button>
+            </td> `
+        tabla.appendChild(fila)
+    });
 }
 
 
 )
 
-btnPdf.addEventListener("click",function(){
-    let productos = JSON.parse(localStorage.getItem("productos"))||[]
-   
-    doc.setFontSize(26);  
-    doc.text("Lista de Productos", 70, 15); 
+btnPdf.addEventListener("click", function () {
+    let productos = JSON.parse(localStorage.getItem("productos")) || []
+
+    doc.setFontSize(26);
+    doc.text("Lista de Productos", 70, 15);
     doc.setFontSize(14);
-    const texto = productos.map(producto=>
-       `Nombre del producto: ${producto.nombre} - Precio: ${producto.precio} - Descripcion: ${producto.descripcion}`
+    const texto = productos.map(producto =>
+        `Nombre del producto: ${producto.nombre} - Precio: ${producto.precio} - Descripcion: ${producto.descripcion}`
     ).join("\n\n")
 
     doc.text(texto, 10, 30);
